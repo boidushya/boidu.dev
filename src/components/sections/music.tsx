47273e6e8cf539c303cf9989709ac8bc449bc9f7
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2024-06-09 05:31:59 +0530

    fix: removed unused import

--- src/components/sections/music.tsx
+++ src/components/sections/music.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const containerVariants = {
   initial: {},
